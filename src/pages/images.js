728230a96a2b31c7f2ca25739b784fcaf45fcf13
haiman232 <haiman.jp@gmail.com>
2021-02-24 01:12:00 +0900

    Navbar component

---
+++ src/pages/images.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const Images = () => (
+  <Layout>
+    <SEO title="Images" />
+    <h1>Images</h1>
+  </Layout>
+)
+
+export default Images
